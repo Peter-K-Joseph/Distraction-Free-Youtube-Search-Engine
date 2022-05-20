d2aa927a2d92bbe6b9e87e7f5a1f8c35d99523ea
Peter K Joseph <69970683+Peter-K-Joseph@users.noreply.github.com>
2022-05-21 00:45:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function search(){
-    let a = "https://www.youtube.com/results?search_query=" + document.getElementById("Search").value;
+    let a = "http://alloy69420.herokuapp.com/web/_aHR0cHM6Ly93d3cueW91dHViZS5jb20=_/results?search_query=" + document.getElementById("Search").value;
     window.location.replace(a);
 }
